cd4835999d78368c01541a5c5cc44e1c1f4e2e0f
brewchetta <chett.tiller@gmail.com>
2020-07-13 13:46:41 -0400

    Added proper seeds

--- 02-ruby-alien-relationships/lib/seed.rb
+++ 02-ruby-alien-relationships/lib/seed.rb
@@ -1,14 +1,13 @@
 puts "Creating seeds..."
 
-p1 = Planet.new
-p2 = Planet.new
-p3 = Planet.new
+p1 = Planet.new("Glorblaff")
+p2 = Planet.new("Zark")
+p3 = Planet.new("Xxax")
 
-a1 = Alien.new
-a2 = Alien.new
-a3 = Alien.new
-a4 = Alien.new
-a5 = Alien.new
-a6 = Alien.new
+a1 = Alien.new("E.T")
+a2 = Alien.new("Chewbacca")
+a3 = Alien.new("Mother Brain")
+a4 = Alien.new("Dr Zoidberg")
+a5 = Alien.new("Yvie Oddly")
 
 puts "Seeds created!"
