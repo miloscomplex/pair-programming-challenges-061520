af4a79775a1e6d991b8dc09831dc4b89868b577a
brewchetta <chett.tiller@gmail.com>
2020-07-13 13:47:09 -0400

    Removed spaceships

--- 02-ruby-alien-relationships/lib/environment.rb
+++ 02-ruby-alien-relationships/lib/environment.rb
@@ -4,7 +4,6 @@ require "pry"
 # Models
 require_relative "./models/Planet.rb"
 require_relative "./models/Alien.rb"
-require_relative "./models/Spaceship.rb"
 
 # Seeds
 require_relative "./seed.rb"
