f320499f0ad086f150b48ab80d23b33bb22ed8a5
miloscomplex <hicallmesutton@gmail.com>
2020-07-16 21:12:09 -0500

    done

--- 01-ruby-objects-build-a-sandwich/sandwich.rb
+++ 01-ruby-objects-build-a-sandwich/sandwich.rb
@@ -3,27 +3,43 @@ require "pry"
 class Sandwich
 
   #dd your code for the sandwich here
-  def initialize(name)
+  def initialize(name, price, ingredients = [], bread = "wheat", isVegetarian, isVegan)
     @name = name
+    @price = price
+    @ingredients = ingredients
+    @bread = bread
+    @isVegetarian = isVegetarian
+    @isVegan = isVegan
+    @ratings = []
   end
 
-  attr_reader :name
-  attr_accessor :price, :ingredients, :bread, :ratings, :vegetarian, :vegan
+  attr_reader :name, :isVegetarian, :isVegan
+  attr_accessor :price, :ingredients, :bread, :ratings, :avg_rating
 
-  @ingredients= []
-  @@ratings = []
+  @@avg_rating = []
+
+  def isVegetarian=(boolean)
+    @isVegetarian = boolean
+    if !boolean
+      @isVegan = false
+    end
+  end
+
+  def isVegan=(boolean)
+    @isVegan = boolean
+    if boolean
+      @isVegetarian = true
+    end
+  end
 
   def order
-    puts "You ordered the #{name} which has #{make_list(ingredients)} on #{bread}. That will be $#{price} please!"
+    "You ordered the #{name} which has #{make_list(ingredients)} on #{bread}. That will be $#{price} please!"
   end
-  #
-  # do we make these boolean methods?
-  # def isVegetarian?(input = false)
-  #   input == true ? true : false
-  # end
 
   def make_list(array)
-    if array.length == 1
+    if array.length == 0
+      "nothing"
+    elsif array.length == 1
       array.join()
     elsif array.length == 2
       array.join(" and ")
@@ -32,49 +48,28 @@ class Sandwich
     end
   end
 
-  def isVegetarian?
-    vegetarian
-  end
 
-  def isVegan?
-    @vegetarian = true if vegan == true
-    vegan
-    # How can you change the setter method to make it so setting
-    # `isVegan?` to true also sets `isVegetarian?` to true?
-  end
-
-
-  def add_rating(rating)
-    @@ratings << rating if rating.between?(1,5)
+  def add_rating(rating_arg)
+    @ratings << rating_arg if rating_arg.between?(1,5)
   end
 
   def average_rating
     # use inject or reduce
-    ratings_sum = @@ratings.inject {|sum, n| sum + n }
-    ratings_avg = ratings_sum / @@ratings.count
+    ratings_sum = ratings.inject {|sum, n| sum + n }
+    ratings_avg = ratings_sum / @ratings.count
   end
 
 end
 
-# Test data modified 
-sandwich_1 = Sandwich.new("turkey club")
-sandwich_1.price = 3.00
-sandwich_1.ingredients = ["mayo","bacon","lettuce","tomato","turkey"]
-sandwich_1.bread = "rye"
+# Test data modified
+sandwich_1 = Sandwich.new("turkey club", 6.99, ["mayo","bacon","lettuce","tomato","turkey"], "rye", false, false)
 sandwich_1.add_rating(3)
 
-sandwich_2 = Sandwich.new("grilled cheese")
-sandwich_2.price = 4.30
-sandwich_2.ingredients = ["swiss cheese","muenster cheese","mayo","butter"]
-sandwich_2.bread = "Italian"
+sandwich_2 = Sandwich.new("grilled cheese", 4.29, ["swiss cheese","muenster cheese","mayo","butter"], "Italian", false, false)
 sandwich_2.add_rating(4.5)
 
-sandwich_3 = Sandwich.new("veggie wrap")
-sandwich_3.price = 5.55
-sandwich_3.ingredients = ["portabella mushrooms", "red peppers", "zucchini", "avacado", "blue cheese"]
-sandwich_3.bread = "spinach wrap"
-sandwich_3.vegetarian = true
-sandwich_2.add_rating(3.8)
+sandwich_3 = Sandwich.new("veggie wrap", 5.55, ["portabella mushrooms", "red peppers", "zucchini", "avacado", "blue cheese"], "spinach wrap", true, false)
+sandwich_3.add_rating(3.8)
 
 binding.pry
 puts "Yes chef!"
